5ef8e5ed7574496cc0c45f575984f45f874cf9c2
Storezhang <storezhang@gmail.com>
2021-06-03 21:48:04 +0800

    解决未使用变量

--- clear.go
+++ clear.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/tencentyun/cos-go-sdk-v5"
 )
 
-func clear(conf *config, _ *cos.Client) (err error) {
-	var enable bool
+func clear(_ *config, _ *cos.Client) (err error) {
+	/*var enable bool
 	if enable, err = strconv.ParseBool(conf.Clear); nil != err || !enable {
 		return
-	}
+	}*/
 
 	// 清理存储桶
 	// client.Bucket.Get()
