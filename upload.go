d07e2244cc47986d1feed0cc18ac7737f55d0ea8
storezhang <storezhang@gmail.com>
2022-12-09 21:54:31 +0800

    feat(日志): 增加上传日志

--- upload.go
+++ upload.go
@@ -11,14 +11,15 @@ import (
 )
 
 func (p *plugin) upload() (undo bool, err error) {
-	var paths []string
-	if paths, err = gfx.All(p.Folder); nil != err {
-		return
-	}
-
-	for _, path := range paths {
-		if err = p.uploadFile(path); nil != err {
-			return
+	if paths, ae := gfx.All(p.Folder); nil != ae {
+		err = ae
+		p.Error("列举目录文件出错", field.New("folder", p.Folder), field.Error(err))
+	} else {
+		p.Debug("即将上传文件", field.New("paths", paths))
+		for _, path := range paths {
+			if err = p.uploadFile(path); nil != err {
+				return
+			}
 		}
 	}
 
@@ -26,8 +27,15 @@ func (p *plugin) upload() (undo bool, err error) {
 }
 
 func (p *plugin) uploadFile(path string) (err error) {
-	var rel string
-	if rel, err = filepath.Rel(p.Folder, path); nil != err {
+	rel := ""
+	pf := field.New("path", path)
+	if _rel, re := filepath.Rel(p.Folder, path); nil != re {
+		err = re
+		p.Error("获取文件相对路径出错", pf, field.Error(err))
+	} else {
+		rel = _rel
+	}
+	if nil != err {
 		return
 	}
 
@@ -42,14 +50,13 @@ func (p *plugin) uploadFile(path string) (err error) {
 	rel = strings.Join(paths, p.Separator)
 	options := new(cos.MultiUploadOptions)
 	options.CheckPoint = true
-	pathField := field.New("path", path)
 	if _, rsp, ue := p.cos.Object.MultiUpload(context.Background(), rel, path, options); nil != ue {
 		err = ue
-		p.Error("上传文件出错", pathField, field.Error(err))
+		p.Error("上传文件出错", pf, field.Error(err))
 	} else if 200 != rsp.StatusCode {
-		p.Warn("上传文件失败", pathField)
+		p.Warn("上传文件失败", pf)
 	} else {
-		p.Debug("文件上传成功", pathField)
+		p.Debug("文件上传成功", pf)
 	}
 
 	return
